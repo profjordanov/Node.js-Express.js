635c3b3534559e79424a35c17e65472efd9e1b29
Jordan Jordanov <d_yugioh@abv.bg>
2017-10-17 19:44:55 +0300

    Messanger.js
    Node.js + MongoDb + Express.js + HTML + CSS

---
+++ Messenger.js/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  development: {
+    port: process.env.PORT || 3000,
+    //TODO: Set the dabase name
+    dbPath: 'mongodb://localhost:27017/demo-server-db'
+  },
+  production: {}
+}
