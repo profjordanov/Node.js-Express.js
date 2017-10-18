9f153bf7e070e9f9d569da7c0b2e8ea21075b24e
Jordan Jordanov <d_yugioh@abv.bg>
2017-10-18 10:01:32 +0300

    Car Renting System
    Car Renting System

---
+++ Car Renting System/server/config/routes.js
@@ -0,0 +1,25 @@
+const controllers = require('../controllers')
+const auth = require('./auth')
+
+module.exports = (app) => {
+  app.get('/', controllers.home.index)
+  app.get('/about', auth.isAuthenticated, controllers.home.about)
+
+  app.get('/users/register', controllers.users.registerGet)
+  app.post('/users/register', controllers.users.registerPost)
+  app.get('/users/login', controllers.users.loginGet)
+  app.post('/users/login', controllers.users.loginPost)
+  app.post('/users/logout', controllers.users.logout)
+  app.get('/users/me', controllers.users.profile)
+
+  app.get('/cars/add', auth.isInRole('Admin'), controllers.cars.addGet)
+  app.post('/cars/add', auth.isInRole('Admin'), controllers.cars.addPost)
+  app.get('/cars/all', controllers.cars.all)
+  app.post('/cars/rent/:id', auth.isAuthenticated, controllers.cars.rent)
+
+  app.all('*', (req, res) => {
+    res.status(404)
+    res.send('404 Not Found!')
+    res.end()
+  })
+}
---
+++ Car Renting System/typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/mongoose/index.d.ts" />
